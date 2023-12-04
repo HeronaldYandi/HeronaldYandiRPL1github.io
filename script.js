eca0f390b29ea731a6718204fb082ea00bc02a35
HeronaldYandi <148335729+HeronaldYandi@users.noreply.github.com>
2023-12-04 14:18:57 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
-const operasi = prompt('Masukan kode ( +, -, *, / )');
-const angka1 = parseFloat(prompt('KETIK ANGKA YANG INGIN ANDA PILIH'));
-const angka2 = parseFloat(prompt('KETIK LAGI ANKGA KE DUA YANG INGIN DIPILIH'));
+while(true) {
+  let playerInput = prompt("Pilih batu, gunting, kertas:");
+  let player = playerInput.toLowerCase(); //Convert input to lowercase
+  let bot = Math.floor(Math.random()*10);
 
-let hasil;
-if (operasi == '+') {
-    hasil = angka1 + angka2
-}
-if (operasi == '-') {
-    hasil = angka1 - angka2
-}
-if (operasi == '*') {
-    hasil = angka1 * angka2
-}
-if (operasi == '/') {
-    hasil = angka1 / angka2
+  if(bot < 3){
+      bot = 'kertas';
+  }else if(bot > 3 && bot <= 6){
+      bot = 'batu';
+  }else{
+      bot = 'gunting';
+  }
+
+  let hasil;
+
+  if(player == bot){
+    hasil = 'Hasil permainan seri';
+  }else if(bot == 'kertas'){
+    if(player == 'gunting'){
+      hasil = 'SELAMAT KAMU MENANG';
+    }else if(player == 'batu'){
+      hasil = 'KAMU KALAH, COBA LAGI';
+    }else{
+      alert("Tidak Dapat Mendeteksi!")
+    }
+  }else if(bot == 'batu'){
+    if(player == 'kertas'){
+      hasil = 'SELAMAT KAMU MENANG';
+    }else if(player == 'gunting'){
+      hasil = 'KAMU KALAH, COBA LAGI';
+    }else{
+      alert("Tidak Dapat Mendeteksi!")
+    }
+  }else if(bot == 'gunting'){
+    if(player == 'batu'){
+      hasil = 'SELAMAT KAMU MENANG';
+    }else if(player == 'kertas'){
+      hasil = 'KAMU KALAH, COBA LAGI';
+    }else{
+      alert("Tidak Dapat Mendeteksi!")
+    }
+  }
+  alert("Ronald memilih "+bot+" sedangkan kamu memilih "+player+" hasil permainan adalah: "+hasil);
 }
 
-document.writeln(`${angka1} ${operasi} ${angka2} = ${hasil}`);
+
